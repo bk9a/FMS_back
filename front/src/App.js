86114d234334d2757768ab5985aeaf9cbfe39819
bay1112 <100332375+bay1112@users.noreply.github.com>
2023-09-10 17:18:26 +0800

    add tailwind config

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import PPAGE from "./Componentsbay/PricePage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>test</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PPAGE></PPAGE>
     </div>
   );
 }
---
+++ front/src/Componentsbay/PricePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const pricePage = () => {
+  console.log("testtt");
+  return (
+    <>
+      <h2 className="bg-red-500">test</h2>
+    </>
+  );
+};
+
+export default pricePage;
