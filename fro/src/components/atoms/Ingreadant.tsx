3bb33e4e1019ceb22cf1a671b6db52ad75160852
bay1112 <100332375+bay1112@users.noreply.github.com>
2023-09-19 00:43:58 +0800

    fucking typescript error-toi changes

--- fro/src/components/atoms/Ingreadant.tsx
+++ fro/src/components/atoms/Ingreadant.tsx
@@ -3,15 +3,19 @@ import { InputField } from "./InputField";
 type IngreadantProps = {
   name: string;
   gram: number;
+  id: number;
   //   Placeholder: string;
-  //   change: (a: number) => void;
+  remove: (a: number) => void;
   //   children?: React.ReactNode;
   //   ContainerClass: string;
   //   value: number;
   //   max?: number;
 } & Omit<React.ComponentProps<"input">, "children">;
 
-export const Ingreadant = ({ name, gram }: IngreadantProps) => {
+export const Ingreadant = ({ name, gram, id, remove }: IngreadantProps) => {
+  const handleSelectChange = () => {
+    remove(id as any);
+  };
   return (
     <div className="w-full flex justify-center items-center">
       <div className="flex">
@@ -19,7 +23,10 @@ export const Ingreadant = ({ name, gram }: IngreadantProps) => {
           <span>{name}</span>
           <span>{gram}gr</span>
         </div>
-        <div className="flex justify-center items-center w-8 h-8 bg-bodyhack cursor-pointer select-none">
+        <div
+          className="flex justify-center items-center w-8 h-8 bg-bodyhack cursor-pointer select-none"
+          onClick={handleSelectChange}
+        >
           <span className="text-gray-700 text-lg">x</span>
         </div>
       </div>
